f95cf7d3fc6d4a8f8241e0a5b004c502f0a333e0
jiork16 <jordan_r_a@hotmail.es>
2022-09-28 11:29:01 -0500

    se agrega clase genericas

---
+++ DesafioJordanRodriguesApiRest.Application/Features/GenericResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioJordanRodriguesApiRest.Application.Features
+{
+    public class GenericResponse
+    {
+        public int Id { get; set; }
+        public string UserName  { get; set; }
+        public string UserNameAdvisor { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+    public class SumariResponse
+    {
+        public decimal Balance { get; set; }
+        public decimal Aportes { get; set; }
+        public DateTime Date { get; set; }
+        public int Userid { get; set; }
+    }
+}
